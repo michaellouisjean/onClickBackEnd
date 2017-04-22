03995cf1db574b2ac9decfc01769bb55b597ecca
Marc Kalil <m.coding@hotmail.com>
2017-04-22 23:09:10 +0200

    Commentaires pouur le chat => serverMessages

--- serverMessages.js
+++ serverMessages.js
@@ -28,98 +28,133 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
 // --- Socket
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+var server = require("http").createServer(app);
+var io = require("socket.io")(server);
 
 var Message = require("./models/Message");
 var User = require("./models/User");
 
-// Store conversation in Users DB
-function insertNewTalk (candidate_id, recruiter_id) {
-  Message.findOne({id_candidate: candidate_id, id_recruiter: recruiter_id})
-    .exec()
-    .then (function (talk) {
-      console.log('insertNewTalk talk._id ', talk._id);
-      User.findOneAndUpdate({_id: candidate_id},
-        {$push: {
-          messages: {id_speaker: recruiter_id, id_message: talk._id}
-        }})
-        .exec();
 
-      User.findOneAndUpdate({_id: recruiter_id},
-        {$push: {
-          messages: {id_speaker: candidate_id, id_message: talk._id}
-        }})
-        .exec();
-        
-      io.emit('serverloadsMessages', talk);
+
+//----------- ECHANGE SERVEUR - CLIENT --------------
+// Ajoute une conversation à la liste des conversations de chaque interlocuteur
+function insertNewTalk(candidate_id, recruiter_id) {
+  // On retrouve la conversation nouvellement créée dans la base de données Message
+  Message.findOne({ id_candidate: candidate_id, id_recruiter: recruiter_id })
+    .exec()
+    .then(function(talk) {
+      // Puis on ajoute cette conversation à la liste des conversations
+      // de chaque interlocuteur
+
+      console.log("insertNewTalk talk._id ", talk._id);
+
+      // Côté candidat
+      User.findOneAndUpdate(
+        { _id: candidate_id },
+        {
+          $push: {
+            messages: { id_speaker: recruiter_id, id_message: talk._id }
+          }
+        }
+      ).exec();
+
+      // Côté recruteur
+      User.findOneAndUpdate(
+        { _id: recruiter_id },
+        {
+          $push: {
+            messages: { id_speaker: candidate_id, id_message: talk._id }
+          }
+        }
+      ).exec();
+
+      // Le serveur envoie la conversation au client
+      io.emit("serverloadsMessages", talk);
     });
 } // insertNewTalk
 
+// Retrouve une conversation existante entre 2 interlocuteurs
+// puis l'envoie au client par le callback
 function setTalk(talk_id, cb) {
-  Message.findById(talk_id)
-  .exec()
-  .then(cb);
+  Message.findById(talk_id).exec().then(cb);
 } // setTalk
 
-io.on('connection', function (client) { // le serveur reçoit tous les évènement du client
-  client.on('clientGetMessages', function (speakers) {
-      console.log('server recieved user', speakers.userId);
-      console.log('server recieved speaker', speakers.speakerId);
-      User.findById(speakers.userId)
-        .where({messages: { $elemMatch:{id_speaker: speakers.speakerId}}})
-        .exec()
-        .then (function (user) {
-          console.log('talk found ');
-          if (user) {
-            let talk_id = null;
-            user.messages.forEach(function(element) {
-              if (speakers.speakerId == element.id_speaker) {
-                talk_id = element.id_message;
-              }
-            }); // forEach
+// le serveur reçoit tous les évènement du client
+io.on("connection", function(client) {
+  // Le client demande à récupérer une conversation entre 2 interlocuteurs
+  client.on("clientGetMessages", function(speakers) {
+    console.log("server recieved user", speakers.userId);
+    console.log("server recieved speaker", speakers.speakerId);
 
-            setTalk(talk_id, (talk) => {
-              console.log('setTalk cb talk ',talk);
-              io.emit('serverloadsMessages', talk);
-            });
-            return;
-          }
-          else {
-            const candidate = speakers.userStatus === 'candidate' ? speakers.userId : speakers.speakerId;
-            const recruiter = speakers.userStatus === 'recruiter' ? speakers.userId : speakers.speakerId;
-
-            // Create a new entry in Message DB
-            const talk = new Message ({
-              id_candidate: candidate,
-              id_recruiter: recruiter,
-              messages: []
-            });
-            talk.save()
-            .then (function() {insertNewTalk (candidate, recruiter)});
-          } // else
-        });
-  });
-
-  client.on('clientSendsMessage', function (messageSent) {
-    console.log('server recieved message ', messageSent);
-    var talk_id = mongoose.mongo.ObjectId(messageSent.talk_id);
-    Message.findByIdAndUpdate(talk_id,
-      {$push: {messages: messageSent.message}})
-    .exec()
-    .then(function () {
-      Message.findById(talk_id)
+    // Le serveur recherche dans la base de données si l'utilisateur loggé a
+    // déjà une conversation avec son interlocuteur en fonctions des id reçus
+    User.findById(speakers.userId) // recherche dans l'entrée de l'utilisateur
+      .where({ messages: { $elemMatch: { id_speaker: speakers.speakerId } } }) // une conversation avec son interlocuteur
       .exec()
-      .then (function (talk) {
-        console.log('AfterPush ', talk.messages);
-        io.emit('serverSendsMessage', talk.messages);
+      .then(function(user) {
+        console.log("talk found ");
+        if (user) { // une conversation existe bel et bien entre les 2 interlocuteurs
+          let talk_id = null;
+          user.messages.forEach(function(element) { // On recherche la conversation dans sa liste de conversations
+            if (speakers.speakerId == element.id_speaker) {
+              talk_id = element.id_message; // On récupère l'id de la conversation
+            }
+          }); // forEach
+
+          // On initie la conversation en cours puis le serveur envoie la liste
+          // des messages au client en callback
+          setTalk(talk_id, talk => {
+            console.log("setTalk cb talk ", talk);
+            io.emit("serverloadsMessages", talk);
+          });
+          return;
+        }
+        // Les 2 interlocuteurs n'ont jamais discuter ensemble
+        else {
+          // On détermine qui est le candidat et qui est le recruteur
+          const candidate = speakers.userStatus === "candidate"
+            ? speakers.userId
+            : speakers.speakerId;
+          const recruiter = speakers.userStatus === "recruiter"
+            ? speakers.userId
+            : speakers.speakerId;
+
+          // On créer un nouvelle entrée dans la base de données Message
+          // qui stocke tous les messages d'une conversation
+          const talk = new Message({
+            id_candidate: candidate,
+            id_recruiter: recruiter,
+            messages: []
+          });
+          talk.save().then(function() {
+            // Puis on ajoute cette conversation à la liste des conversations
+            // de chaque interlocuteur
+            insertNewTalk(candidate, recruiter);
+          });
+        } // else
       });
-    });
-  });
+  }); // clientGetMessages
+
+  // Le client envoie un message
+  client.on("clientSendsMessage", function(messageSent) {
+    console.log("server recieved message ", messageSent);
+    var talk_id = mongoose.mongo.ObjectId(messageSent.talk_id);
+
+    // On stocke le message dans la conversation
+    // puis le serveur envoie au client la liste des messages mise à jour
+    Message.findByIdAndUpdate(talk_id, {
+      $push: { messages: messageSent.message }
+    })
+      .exec()
+      .then(function() {
+        Message.findById(talk_id).exec().then(function(talk) {
+          // le serveur envoie au client la liste des messages mise à jour
+          io.emit("serverSendsMessage", talk.messages);
+        });
+      });
+  }); //clientSendsMessage
 });
 
-
-
-server.listen(3000, function () {
-  console.log('serverMessage listen 3000');
+server.listen(process.env.PORT, function() {
+  console.log("serverMessage listen ", process.env.PORT);
 });
